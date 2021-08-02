14538cdb3987ed4adea8cede0141b37159ba5340
bazz666 <herrera.se1992@gmail.com>
2021-08-02 01:04:32 -0400

    add percentage

---
+++ app/models/strain.rb
@@ -0,0 +1,11 @@
+class Strain < ApplicationRecord
+    
+    has_many :wine_strains
+    has_many :wines, through: :wine_strains
+
+    validates :name, presence: true
+    
+    def to_s
+        name
+    end
+end
---
+++ app/models/wine.rb
@@ -0,0 +1,8 @@
+class Wine < ApplicationRecord
+    
+    has_many :wine_strains
+    has_many :strains, through: :wine_strains
+  
+    validates :name, presence: true
+  
+end
